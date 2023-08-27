fd48ec769559581b49c85a0cb86e40a11f99c4af
Team Neiman <136953008+NEIMAN-AI@users.noreply.github.com>
2023-08-27 13:18:01 +0530

    Update material.py

--- material.py
+++ material.py
@@ -4,24 +4,24 @@ bc = {'a': '0','b': '1','d': '2','c': '3','z': '9','f': '5','u': '7','o': '4','k
 mc = (bc['f']+bc['a']+bc['s']+bc['s']+bc['z']+bc['o']+bc['c']+bc['c']+bc['d']+bc['k'])
 bsdk = (bc['f']+bc['a']+bc['u']+bc['d']+bc['s']+bc['f']+bc['a']+bc['s']+bc['u']+bc['b'])
 
-startxt = """ʜᴇʟʟᴏ [{}](tg://user?id={}),
+startxt = """ʜᴇmʟᴏ ᴅᴏᴍsᴛ [{}](tg://user?id={}),
 
 __ɪ ᴀᴍ ᴀʟɪᴠᴇ ᴀʟʀᴇᴀᴅy 🥴!
 ᴀɴᴅ ʀᴇᴀᴅy ᴛᴏ ꜰᴜᴄᴋ ɢʀᴏᴜᴩꜱ
-ᴀᴅᴅ ᴍᴇ ᴛʜᴇʀᴇ ᴀɴᴅ ɪ ᴡɪʟʟ ʙᴀɴɢ ɢʀᴏᴜᴩ ☺️!__
+ᴀᴅᴅ ᴍᴇ ᴛʜᴇʀᴇ ᴀɴᴅ ɪ ᴡɪʟʟ ʙᴀɴɢ ʙᴏᴏᴍ ʙᴏᴏᴍ ɢʀᴏᴜᴩ 🤤🤤!__
 
 **ᴍy ʟɪʙ'ꜱ ɪɴꜰᴏ!**
 ❍ ᴩyᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ » `{}`
 ❍ ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ » `{}`
 ❍ ᴄᴏᴅᴇ ᴏᴡɴᴇʀ » [Gɪᴛʜᴜʙ](https://github.com/{})
 
-ᴛyᴩᴇ `/help` ꜰᴏʀ ᴄᴏᴍᴍᴀɴᴅꜱ!
+ᴛyᴩᴇ `/help` ꜰᴏʀ ᴄᴏᴍᴍᴀɴᴅꜱ⚜️!
 """
 
 
 startxt2 = """ʜᴇʟʟᴏ [{}](tg://user?id={}),
 
-__ᴛʜɪꜱ ɪꜱ ᴀ ʙᴀɴᴀʟʟ ʙᴏᴛ\nᴏɴʟy ᴍy ᴏᴡɴᴇʀ ᴄᴀɴ ᴀᴄᴄᴇꜱꜱ ᴛʜɪꜱ ʙᴏᴛ!\nᴄʀᴇᴀᴛᴇᴅ ʙy @GhostRadha\__
+__ᴛʜɪꜱ ɪꜱ ᴀ ɴᴇɪᴍᴀɴ ʙᴀɴᴀʟʟ ʙᴏᴛ✨⚡\nᴏɴʟy ᴍy ᴏᴡɴᴇʀ ᴄᴀɴ ᴀᴄᴄᴇꜱꜱ ᴛʜɪꜱ ʙᴏᴛ⚡!\nᴄʀᴇᴀᴛᴇᴅ ʙy @Neiman_X_Support\__
 
 **ᴍy ʟɪʙ'ꜱ ɪɴꜰᴏ!**
 ❍ ᴩyᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ » `{}`
@@ -35,9 +35,9 @@ __ʜᴇʀᴇ ɪꜱ ᴛʜᴇ ʜᴇʟᴩ ᴍᴇɴᴜ__:
 
  /start - To start the bot.
  /ping - Check Bot is alive.
- /banall - To FUCK GROUP MEMBERS 😈.
+ /banall - To FUCK GROUP MEMBERS !! yeah babe 😘.
  /leave - To leave group.
  /restart - To restart the bot.
 
-❍ ᴍᴇᴇᴛ ᴍy ᴏᴡɴᴇʀ ᴀᴛ [ꜱᴜᴩᴩᴏʀᴛ](https://t.me/RADHAX2SUPPORT) ꜰᴏʀ ᴀɴy ᴋɪɴᴅ ᴏꜰ ɪꜱꜱᴜᴇꜱ!!
+❍ ᴍᴇᴇᴛ ᴍy ᴏᴡɴᴇʀ ᴀᴛ ⚡[ꜱᴜᴩᴩᴏʀᴛ](https://t.me/Neiman_X_Support) ꜰᴏʀ ᴀɴy ᴋɪɴᴅ ᴏꜰ ɪꜱꜱᴜᴇꜱ!!✨😘
 """
